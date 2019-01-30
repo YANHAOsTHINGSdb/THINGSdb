97ebe227153b560b39dfb6668917300d055ba31b
YANHAOsTHINGSdb <47000444+YANHAOsTHINGSdb@users.noreply.github.com>
2019-01-30 18:15:03 +0900

    20190130

--- src/main/java/stage3/NLP/StanfordCoreNlpDemoChinese.java
+++ src/main/java/stage3/NLP/StanfordCoreNlpDemoChinese.java
@@ -53,7 +53,8 @@ public class StanfordCoreNlpDemoChinese {
 		if (args.length > 0) {
 			document = new Annotation(IOUtils.slurpFileNoExceptions(args[0]));
 		} else {
-			document = new Annotation("克林顿说，华盛顿将逐步落实对韩国的经济援助。金大中对克林顿的讲话报以掌声：克林顿总统在会谈中重申，他坚定地支持韩国摆脱经济危机。");
+//			document = new Annotation("克林顿说，华盛顿将逐步落实对韩国的经济援助。金大中对克林顿的讲话报以掌声：克林顿总统在会谈中重申，他坚定地支持韩国摆脱经济危机。");
+			document = new Annotation("自然语言是人类思维与交流的主要工具，是人类智慧的结晶。");
 		}
 
 		pipeline.annotate(document);
